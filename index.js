5ad97a86dceda316ed8d0eb2f457a3a332f6c01b
DanielGG1024 <a22472510@gmail.com>
2021-11-05 23:22:01 +0800

    fix bug logo

--- index.js
+++ index.js
@@ -3,14 +3,33 @@ const mainHeader = document.querySelector('#main-header')
 const logo = mainHeader.querySelector('.logo')
 const body = document.querySelector('#body')
 
+if(body.offsetWidth >= 1024){
+    logo.classList.add('d-none')
+    logo.classList.remove('opacity-show')
+    logo.classList.remove('opacity-zero')
+}
 window.addEventListener('scroll', function () {
-    if(body.offsetWidth > 1024){
+    if (body.offsetWidth >= 1024) {
         if (this.scrollY >= 250) {
-            secondaryHeader.classList.add('zero')
-            logo.style.opacity = '1';
+            secondaryHeader.classList.add('zero-height')
+            logo.classList.add('opacity-show')
+            logo.classList.remove('opacity-zero')
+            logo.classList.remove('d-none')
         } else if (this.scrollY < 250) {
-            secondaryHeader.classList.remove('zero')
-            logo.style.opacity = '0';
+            logo.classList.add('d-none')
+            secondaryHeader.classList.remove('zero-height')
+            logo.classList.add('opacity-zero')
+            logo.classList.remove('opacity-show')
         }
-    }
+    } 
 })
+
+
+window.onresize = function(event) {
+    console.log('這到底是三小')
+    if(window.body.offsetWidth <= 1024){
+        logo.classList.add('d-none')
+        logo.classList.remove('opacity-show')
+        logo.classList.remove('opacity-zero')
+    }
+}
